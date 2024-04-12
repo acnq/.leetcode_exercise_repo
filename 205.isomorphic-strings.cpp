6366898cac6653a781e67ff272d71ea9ecfc132f
acnq <fanyuanhaotc@163.com>
2024-04-12 22:54:48 +0300

    daily commit

---
+++ 205.isomorphic-strings.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=205 lang=cpp
+ * @lcpr version=30122
+ *
+ * [205] 同构字符串
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> s2t, t2s;
+        int len = s.size();
+        for (int i = 0; i < len; i++)
+        {
+            char x = s[i], y = t[i];
+            if ((s2t.count(x) && s2t[x] != y) || (t2s.count(y) && t2s[y] != x))
+            {
+                return false;
+            }
+            s2t[x] = y;
+            t2s[y] = x;
+        }
+        return true;   
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "egg"\n"add"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "foo"\n"bar"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "paper"\n"title"\n
+// @lcpr case=end
+
+ */
+
