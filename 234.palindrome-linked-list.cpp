fd39645549405f2e8a29d98178f3b485441866da
acnq <fanyuanhaotc@163.com>
2024-04-24 01:16:27 +0300

    daily commit

--- 234.palindrome-linked-list.cpp
+++ 234.palindrome-linked-list.cpp
@@ -45,25 +45,72 @@ struct ListNode {
 
 class Solution {
 public:
-    ListNode* frontEnd;
-    bool recursivelyCheck(ListNode* head){
-        if (head != nullptr)
+    // ListNode* frontEnd;
+    // bool recursivelyCheck(ListNode* head){
+    //     if (head != nullptr)
+    //     {
+    //         if (!(recursivelyCheck(head->next)))
+    //         {
+    //             return false;   
+    //         }
+    //         if (head->val != frontEnd->val)
+    //         {
+    //             return false;
+    //         }
+    //         frontEnd = frontEnd->next;
+    //     }
+    //     return true;
+    // }
+    ListNode* partiList(ListNode* head){
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast->next != nullptr && fast->next->next != nullptr)
         {
-            if (!(recursivelyCheck(head->next)))
-            {
-                return false;   
-            }
-            if (head->val != frontEnd->val)
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+    ListNode* reverseList(ListNode* head){
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+        while (curr != nullptr)
+        {
+            ListNode* tmp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        if (head == nullptr)
+        {
+            return true;
+        }
+        // 区分前半部分和后半部分链表
+        ListNode* plist = partiList(head);
+        // 翻转后半部分的链表
+        ListNode* newplist = reverseList(plist->next);
+
+        ListNode* p1 = head;
+        ListNode* p2 = newplist;
+        bool result = true;
+        while (result && p2 != nullptr)
+        {
+            if (p1->val != p2->val)
             {
                 return false;
             }
-            frontEnd = frontEnd->next;
+            p1 = p1->next;
+            p2 = p2->next;
         }
-        return true;
-    }
-    bool isPalindrome(ListNode* head) {
-        frontEnd = head;
-        return recursivelyCheck(head);
+        result = true;
+        // 还原链表
+        plist->next = reverseList(newplist);
+        return result;
+        // frontEnd = head;
+        // return recursivelyCheck(head);
         // vector<int> nums;
         // ListNode* p = head;
         // while (p != nullptr)
